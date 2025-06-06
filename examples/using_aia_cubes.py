80841c8a0f7481855664057a12c92dfca0024550
Nabil Freij <nabil.freij@gmail.com>
2025-06-06 15:29:18 -0700

    tweak of text

--- examples/using_aia_cubes.py
+++ examples/using_aia_cubes.py
@@ -51,7 +51,7 @@ aia_collection = read_files(sdo_aia_file, memmap=False)
 print(aia_collection)
 
 ###############################################################################
-# We will then select the 304.
+# We will then select the 304 bandpass cube.
 
 print(aia_collection["304_THIN"])
 
@@ -61,8 +61,9 @@ print(aia_collection["304_THIN"])
 # You can also change the axis labels and ticks if you so desire.
 # `WCSAxes provides us an API we can use. <https://docs.astropy.org/en/stable/visualization/wcsaxes/index.html>`__
 
-# Note that the .get_animation() is used to animate this example and is not required normally.
 fig = plt.figure()
+# Note that the .get_animation() is used to animate this example
+# and is not required normally.
 aia_collection["304_THIN"].plot(fig=fig).get_animation()
 
 plt.show()
